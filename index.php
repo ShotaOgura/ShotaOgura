01460112a262e5c1e1121b44a2eb83105f8d12cb
Shota <andy.1125.starrrrrrr@gmail.com>
2021-01-03 12:01:18 +0900

    掲示板機能の作成

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
 <html>
 <header>
 <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
-<title>計算してくれるサイトっぽい</title>
+<title>1ちゃんねる</title>
 </header>
     <body>
-
-
-<div style="text-align:center">
-    <form action="result.php" method="post">
-        <h5>生年月日を入力してください。</h5><br>
-        <!-- 年月日の形式で入力させる -->
-        <input type="date" name="birthday">
-        <input type="submit" value="送信">
-    </form>
-</div>
+        <h3>1ちゃんねる</h3>
+        <div style="margin-left: auto;margin-right: auto;"></div>
+            <li class="flame post">
+                    今日はいい天気ですね<br>
+                    2021/1/3 11:17 おぐらくん<br>
+            </li>
+            <li class="flame post">
+                    こんにちは<br>
+                    <div class="dt">2021/1/3 11:44</div><div class="name"> あだちくん</div><br>
+            </li>
+        </div>
+    <link rel="stylesheet" href="board.css">
     </body>
 </html>
