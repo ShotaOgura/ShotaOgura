b1397a631222fbf0767855e19a2bb9551e6f8c4b
Shota <andy.1125.starrrrrrr@gmail.com>
2021-01-03 11:07:52 +0900

    年齢計算機能をbirthday.phpに移動

---
+++ birthday.php
@@ -0,0 +1,18 @@
+<html>
+<header>
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+<title>計算してくれるサイトっぽい</title>
+</header>
+    <body>
+
+
+<div style="text-align:center">
+    <form action="birthday_result.php" method="post">
+        <h5>生年月日を入力してください。</h5><br>
+        <!-- 年月日の形式で入力させる -->
+        <input type="date" name="birthday">
+        <input type="submit" value="送信">
+    </form>
+</div>
+    </body>
+</html>
---
+++ birthday_result.php
@@ -0,0 +1,36 @@
+<html>
+    <header>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+        <title>計算してくれるサイトっぽい</title>
+    </header>
+    <body>
+
+<?php
+    // POSTで受け取った情報（文字列）を$birthdayに格納
+    $birthday = $_POST['birthday'];
+    
+
+    // $birthdayの文字列を元に、DateTimeクラスの情報を作成？変換？
+    $birthdayDt = new DateTime($birthday);
+    
+
+    // 現在時刻をDateTimeクラスで取得
+    $now = new DateTime();
+    
+    $dateDiff = $now->diff($birthdayDt);
+    $diffYearsStr = $dateDiff->format('%y 歳 %m ヶ月 %d 日');
+?>
+<h5>
+    入力された生年月日 : <?php echo $birthdayDt->format("Y年m月d日") ?>
+    <br>
+    現在 : <?php echo $now->format("Y年m月d日") ?>
+    <br>
+    あなたは、<?php echo $diffYearsStr ?> です!!!
+</h5>
+
+<form action="birthday.php" method="get">
+<input type="submit" value="戻る">
+</form>
+
+    </body>
+</html>
