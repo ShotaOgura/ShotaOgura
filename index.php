b282c7174aac94f7ab0dfff7fcd36b456494882d
Shota <andy.1125.starrrrrrr@gmail.com>
2021-01-03 12:29:18 +0900

    投稿フォームの作成(暫定)

--- index.php
+++ index.php
@@ -4,16 +4,26 @@
 <title>1ちゃんねる</title>
 </header>
     <body>
-        <h3>1ちゃんねる</h3>
-        <div style="margin-left: auto;margin-right: auto;"></div>
-            <li class="flame post">
+        <h3 style="text-align:center">1ちゃんねる</h3>
+        <div class="post-history">
+            <li class="frame post">
                     今日はいい天気ですね<br>
-                    2021/1/3 11:17 おぐらくん<br>
+                    <div class="post dt">2021/1/3 11:17</div><div class="post name">おぐらくん</div><br>
             </li>
-            <li class="flame post">
+            <li class="frame post">
                     こんにちは<br>
-                    <div class="dt">2021/1/3 11:44</div><div class="name"> あだちくん</div><br>
+                    <div class="post dt">2021/1/3 11:44</div><div class="post name"> あだちくん</div><br>
             </li>
+        </div>
+
+        <div class="post-form">
+        <form action="createPost.php" method="post">
+            <label id="userName">名前</label>
+            <input id="userName" type="text" name="userName" maxlength="6">
+            <br>
+            <label id="contents">投稿内容</label>
+            <input id="contents" type="textarea" name="contents" rows="5">
+        </form>
         </div>
     <link rel="stylesheet" href="board.css">
     </body>
